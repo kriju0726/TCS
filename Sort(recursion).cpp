e0429288cc1a353984887361e9b48757063a619b
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2026-03-01 10:38:35 +0530

    Sorting using Recursion

---
+++ Sort(recursion).cpp
@@ -0,0 +1,47 @@
+// Sorting using Recursion...
+#include<bits/stdc++.h>
+using namespace std;
+
+void insert(stack<int>& s, int e){
+    if(s.size() == 0  ||  s.top() >= e){
+        s.push(e);
+        return ;
+    } 
+    
+    int temp = s.top();
+    s.pop();
+    insert(s, e);
+    s.push(temp);
+}
+
+
+void sorts1(stack<int>& s){
+    int n= s.size();
+    if(n == 1) return;
+    
+    int temp = s.top();
+    s.pop();
+    sorts1(s);
+    insert(s, temp);
+}
+
+
+int main(){
+    stack<int> s;
+    s.push(3);
+    s.push(5);
+    s.push(1);
+    s.push(9);
+    s.push(0);
+    s.push(7);
+    int n = s.size();
+    
+    sorts1(s);
+    cout<<"After Sorting.: ";
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    
+    return 0;
+}
