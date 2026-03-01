02f2d5a6a0c7c780d0e60e3ca342b2f21babd76d
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2026-03-01 13:47:20 +0530

    Permutation with Case Change

---
+++ Subset(v2).cpp
@@ -0,0 +1,46 @@
+// Permutation with Case Change...
+
+/*
+Enter your String...: ab
+AB
+Ab
+aB
+ab
+*/
+
+#include<bits/stdc++.h>
+using namespace std; 
+
+void print(string ip, string op){
+    if(ip.length() == 0){
+        cout<<op<<"\n";
+        return ;
+    }
+    
+    string op1 = op;
+    string op2 = op;
+    
+    op1.push_back(toupper(ip[0]));
+    op2.push_back(tolower(ip[0]));
+    
+    
+    ip.erase(ip.begin() + 0);
+    
+    print(ip, op1);
+    print(ip, op2);
+}
+
+
+int main(){
+    string ip, op = "";
+    cout<<"Enter your String...: ";
+    cin>>ip;
+    
+    
+    print(ip, op);
+    
+    return 0;
+}
+
+
+
