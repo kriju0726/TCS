e4be3ee49d1287d916ca62941457e66cb724180a
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2026-03-02 10:09:20 +0530

    Subset Sum using Memotization Approach

---
+++ SubSetSum(Memotsn).cpp
@@ -0,0 +1,36 @@
+// To Check Subset Sum exist or Not(Memotisation Approach)...
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int t[101][101];
+
+bool check(vector<int>& nums, int sum, int n){
+    if(sum == 0) return true;
+    
+    if(n == 0) return false;
+    
+    if(t[n][sum] != -1) return t[n][sum];
+    
+    if(nums[n-1] <= sum){
+        return t[n][sum] = (check(nums, sum - nums[n-1], n-1) || check(nums, sum , n-1));
+    }else{
+        return t[n][sum] = check(nums, sum, n-1);
+    }
+    
+    return 0;
+}
+
+int main(){
+    vector<int> nums = {2,3,7,8,10};
+    memset(t, -1, sizeof(t));
+    int sum = 28;
+    int n = nums.size();
+    
+    if(check(nums, sum, n)) cout<<"Exist\n";
+    else cout<<"Not Exist..";
+    
+    return 0;
+}
+
+
