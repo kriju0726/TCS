6a695dd887d3336c94aed106c14274825a8916cb
kumar.krish07022002@gmail.com <kumar.krish07022002@gmail.com>
2026-03-03 13:42:44 +0530

    UnBounded KnapSack(Coin Change I) Using Recursive Approach

---
+++ CoinChange1(Rcrsn).cpp
@@ -0,0 +1,20 @@
+// Now Using Recursion  UnBounded KnapSack(Coin Change(find max. no. of ways))...
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>& nums, int sum, int n){
+    if(n==0) return 0;
+    if(sum==0) return 1;
+    
+    if(nums[n-1] <= sum) return solve(nums, sum-nums[n-1], n) + solve(nums, sum, n-1);
+    else return solve(nums, sum, n-1);
+}
+
+int main(){
+    vector<int> nums = {1,5,10};
+    int sum = 0, n = nums.size();
+    
+    cout<<"Max Profit..: "<<solve(nums, sum, n);
+    
+    return 0;
+}
